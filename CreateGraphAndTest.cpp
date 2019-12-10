0f503f1be53148c6e36842b80d8196fd8f81924e
Naveed Naqi <nnaqi5534@gmail.com>
2019-12-10 12:21:17 -0500

    Finished the Graph clas and added nice printing functionality

--- CreateGraphAndTest.cpp
+++ CreateGraphAndTest.cpp
@@ -2,6 +2,82 @@
 #include <fstream>
 #include <sstream>
 
+int StringToInt(std::string some_string) {
+    std::stringstream conversion(some_string);
+    int val = 0;
+    conversion >> val;
+    return val;
+}
+
+float StringToFloat(std::string some_string) {
+    std::stringstream conversion(some_string);
+    float val = 0;
+    conversion >> val;
+    return val;
+}
+
+
+
+/*
+** Assumes that the first line of the text file being read is the size of the input graph.
+** @param: input_graph is a text file that contains an adjacencny list representation of a graph.
+*/
+std::size_t GetSize(std::string input_graph) {
+    std::ifstream input_file;
+    input_file.open("InputFiles/"+input_graph);
+
+    std::string curr_line = "";
+    std::getline(input_file, curr_line);
+
+    return StringToInt(curr_line);
+}
+
+void CreateEdges(Graph<int>& new_graph, std::string curr_line) {
+
+    int source_val = StringToInt(curr_line.substr(0,1));
+
+    if(new_graph.FindVertex(source_val) == nullptr) {
+        new_graph.AddVertex(source_val);
+    }
+
+
+    std::size_t size = curr_line.size();
+    if(size < 3) { return; }
+
+    int start_pos = 2;
+    int num_spaces = 0;
+    int vertex_val = 0;
+    float weight = 0;
+
+    for(int i = start_pos; i < size; i++) {
+        
+        if(curr_line[i] == ' ') {
+            num_spaces++;
+
+            if(num_spaces == 1) {
+
+                vertex_val = StringToInt(curr_line.substr(start_pos, i-start_pos+1));
+
+                if (new_graph.FindVertex(vertex_val) == nullptr) {
+                    new_graph.AddVertex(vertex_val);
+                }
+                start_pos = i+1;
+            }
+
+            if(num_spaces == 2) {
+                
+                weight = StringToFloat(curr_line.substr(start_pos, i-start_pos+1));
+                new_graph.AddEdge(source_val, vertex_val, weight);
+                start_pos = i+1;
+                num_spaces = 0;
+            }
+        }
+    }
+
+    weight = StringToFloat(curr_line.substr(start_pos, size-start_pos+1));
+    new_graph.AddEdge(source_val, vertex_val, weight);
+
+}
 
 /*
 ** @param: input_graph is a text file that contains an adjacencny list representation of a graph.
@@ -9,7 +85,8 @@
 Graph<int> CreateGraph(std::string input_graph) {
 
     std::ifstream input_file;
-    Graph<int> new_graph;
+    std::size_t graph_size = GetSize(input_graph);
+    Graph<int> new_graph(graph_size);
 
     input_file.open("InputFiles/"+input_graph);
 
@@ -25,8 +102,8 @@ Graph<int> CreateGraph(std::string input_graph) {
 
         if(line_num != 1) {
 
-            //std::stringstream string_to_int(std::string(line_num[0]));
-            std::cout << curr_line[0] << "\n";
+            //std::cout << curr_line[0] << "\n";
+            CreateEdges(new_graph, curr_line);
         }
 
         line_num++;
@@ -48,4 +125,7 @@ int main(int argc, char **argv) {
     const std::string input_query = std::string(argv[2]);
 
     Graph<int> new_graph = CreateGraph(input_graph);
+    new_graph.PrintAdjList();
+
+    return 0;
 }
--- Graph.hpp
+++ Graph.hpp
@@ -1,71 +1,106 @@
+/*
+** Author: Naveed Naqi
+** Date: 12/10/2019
+** Description: This file is the implementation of a graph. The graph is represented as an adjancey list.
+*/
+
 #include <iostream>
-#include <forward_list>
+#include <list>
 #include <vector>
+#include <unordered_map>
+
+template <typename Object>
+struct Vertex;
+
+template <typename Object>
+struct Edge;
+
+template <typename Object>
+struct Edge {
+
+    Edge( Vertex<Object>* dest, float weight) : dest_(dest), weight_(weight) {}
+    Vertex<Object>* dest_;
+    float weight_;
+
+    void Print() const {
+        std::cout << "-> " << dest_->val_ << "(" << weight_ << ") \t";
+    }
+};
 
 template <typename Object> 
 struct Vertex {
 
-    Vertex(Object val, int weight = -1, Vertex* next = nullptr) : val_(val), weight_(weight), next_(next) {}
-
-    Vertex* next_;
+    Vertex(const Object& val) : val_(val), indegree_(0) {}
     Object val_;
-    int weight_;
+    std::size_t indegree_;
+    std::vector<Edge<Object>> edges_;
+
+    void PrintEdges() const {
+
+        for(auto edge : edges_) {
+            edge.Print();
+        }
+
+        std::cout << "\n";
+    }
 };
 
 template <typename Object> 
 class Graph {
 
 public: 
+    Graph(std::size_t size) {
+        adj_list_.reserve(size);
+    }
 
-    void AddEdge(const Object& source, const Object& destination, int weight);
+    /*
+    ** Adds an edge between two verticies, with the assigned weight. The edge is directed from the source to the destination. 
+    ** Assumes the verticies are already in the graph.
+    ** @param: source is some Object that is already a vertex in the graph
+    ** @param: destination is some Object that is already a vertex in the graph
+    ** @param: weight is is some float that is the weight between the two verticies.
+    */
+    void AddEdge(const Object& source, const Object& destination, float weight);
     void AddVertex(const Object& new_elem);
+    
+    Vertex<Object>* FindVertex(const Object& val_to_search_for);
 
     void PrintAdjList() const;
 
 private:
 
-    void TraverseList(Vertex<Object>* curr_vertex) const;
-    std::vector<Vertex<Object>*> adj_list_;
+    std::unordered_map<Object, Vertex<Object>*> adj_list_;
 };
-
-template <typename Object> 
-void Graph<Object>::TraverseList(Vertex<Object>* curr_vertex) const {
-
-    while(curr_vertex != nullptr) {
-        std::cout << "Value: " << curr_vertex->val_ << "\n";
-        std::cout << "Cost: " << curr_vertex->weight_ << "\n";
-        curr_vertex = curr_vertex->next_;
-    }
-}
 
 template <typename Object> 
 void Graph<Object>::AddVertex(const Object& new_elem) {
 
     Vertex<Object>* new_vertex = new Vertex<int>(new_elem);
-    adj_list_.push_back(new_vertex);
+    adj_list_[new_elem] = new_vertex;
 }
 
 template <typename Object> 
-void Graph<Object>::AddEdge(const Object& source, const Object& destination, int weight) {
+void Graph<Object>::AddEdge(const Object& source, const Object& destination, float weight) {
 
-    for(auto curr_vertex : adj_list_) {
 
-        if(curr_vertex->val_ == source) {
-            
-            Vertex<Object>* new_vertex = new Vertex<Object> (destination, weight);
-            Vertex<Object>* temp = curr_vertex->next_;
+    Vertex<Object>* source_vertex = adj_list_[source];
+    Vertex<Object>* destination_vertex = adj_list_[destination];
+    Edge<Object> new_edge(destination_vertex, weight);
 
-            curr_vertex->next_ = new_vertex;
-            new_vertex->next_ = temp;
-            return;
-        }
-    }
+    source_vertex->edges_.push_back(new_edge);
+}
+
+template <typename Object>
+Vertex<Object>* Graph<Object>::FindVertex(const Object& val_to_search_for) {
+
+    return adj_list_.find(val_to_search_for) == adj_list_.end() ? nullptr : adj_list_[val_to_search_for];
 }
 
 template<typename Object>
 void Graph<Object>::PrintAdjList() const {
 
-    for(auto curr_vertex : adj_list_) {
-        TraverseList(curr_vertex);
+    for(auto itr = adj_list_.begin(); itr != adj_list_.end(); ++itr) {
+        std::cout << itr->first << " : "; 
+        itr->second->PrintEdges();
     }
 }
